6cd027c945dc1bc0e008422ef612e9e013853cea
juanca2703 <jc.aponte2703@gmail.com>
2022-10-27 13:40:38 -0500

    Agregando interaccion en menu desplegable mobile

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const mobileHamMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
+/* Escuchando eventos de click del usuario */
 menuEmail.addEventListener("click", toggleDesktopMenu);
+mobileHamMenu.addEventListener("click", toggleMobileMenu);
 
+/*Funciones interactivas de menus desplegables desktop y mobile */
 function toggleDesktopMenu() {
-
     desktopMenu.classList.toggle("inactive");
+}
 
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("inactive");
 }
