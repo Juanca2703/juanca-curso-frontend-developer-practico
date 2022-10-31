8aa1bb9a3be0129ca580b19b70c864188466df38
juanca2703 <jc.aponte2703@gmail.com>
2022-10-31 15:39:31 -0500

    Agregand interaccion carrito de compras y solucioando conflictos de superposiciones con menus

--- main.js
+++ main.js
@@ -1,17 +1,43 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const mobileHamMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const asideShoppingCar = document.querySelector(".product-detail");
 
 /* Escuchando eventos de click del usuario */
 menuEmail.addEventListener("click", toggleDesktopMenu);
 mobileHamMenu.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoMenu);
 
 /*Funciones interactivas de menus desplegables desktop y mobile */
 function toggleDesktopMenu() {
+
+    const isCarritoClosed = asideShoppingCar.classList.contains("inactive");
+    if(!isCarritoClosed){
+        asideShoppingCar.classList.add("inactive");
+    }
+
+
     desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
+
+    const isCarritoClosed = asideShoppingCar.classList.contains("inactive");
+    if(!isCarritoClosed){
+        asideShoppingCar.classList.add("inactive");
+    }
+
     mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarritoMenu() {
+
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+     if(!isMobileMenuClosed){
+        mobileMenu.classList.add("inactive");
+     }
+
+    asideShoppingCar.classList.toggle("inactive");
 }
